cbd285f189296fdd7605a2630862a23503a32d15
keyeze <keyeze@126.com>
2017-11-29 15:36:13 +0800

    文章系统类搭建,回复被覆盖文件

---
+++ OverrideBlog-article/src/main/java/com/blog/web/controller/ArticleController.java
@@ -0,0 +1,20 @@
+package com.blog.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 文章访问层接口
+ *
+ * @author Flandre#CtChan
+ */
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public Object test() {
+        return "article/test#GET";
+    }
+}
