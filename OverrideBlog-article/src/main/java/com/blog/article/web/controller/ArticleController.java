289b978ed6619252fc2eed83ee374e0d4cb2e71d
keyeze <keyeze@126.com>
2017-11-29 16:07:48 +0800

    配置统一Spring.xml文件

--- OverrideBlog-article/src/main/java/com/blog/article/web/controller/ArticleController.java
+++ OverrideBlog-article/src/main/java/com/blog/article/web/controller/ArticleController.java
@@ -1,4 +1,4 @@
-package com.blog.web.controller;
+package com.blog.article.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
